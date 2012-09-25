627cec99925f1c0bbb69f173a424a846be4ff0df
Tyler Parker <btylerparker@gmail.com>
2012-09-25 18:45:36 -0400

    barebones and some data

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "shadowPlay.h"
+#include "ofxFensterManager.h"
+#include "ofAppGlutWindow.h"
+
+//--------------------------------------------------------------
+int main(){
+	ofAppGlutWindow window; // create a window
+	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
+	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
+	ofRunApp(new shadowPlay()); // start the app
+}
---
+++ src/shadowPlay.cpp
@@ -0,0 +1,61 @@
+#include "shadowPlay.h"
+
+//--------------------------------------------------------------
+void shadowPlay::setup(){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::update(){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::draw(){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void shadowPlay::dragEvent(ofDragInfo dragInfo){ 
+
+}
